bbd200e81c779275d5685671cefe9e01c56e372e
Olamilekan Aremu <aremu0235@gmail.com>
2025-08-06 12:49:51 +0100

    footer-tweaks

--- Components/Footer.js
+++ Components/Footer.js
@@ -6,7 +6,7 @@ import { CiMail } from "react-icons/ci";
 export default function Footer(){
   return(
     <div className="container font-mono text-center mt-10 p-5 md:p-10 text-white">
-          <p className="nav-list">Copyright © 2025 Olamilekan Aremu - Sagittaerys!⚡.</p>
+          <p className="nav-list">Copyright © 2025 Olamilekan Aremu -  <span className="test-font">Sagittaerys!⚡.</span></p>
 
           <div className="flex justify-center gap-4 mt-5">
             <a href="mailto:aremu0235@gmail.com"
